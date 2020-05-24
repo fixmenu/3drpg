7fc4db5793fc5adde4d8f52f1113462e6ba95a28
mert gonkesen <mertgnksn@gmail.com>
2020-05-24 11:06:56 +0300

    first commit

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(0))
+        {
+            moveToCursor();
+        }
+        
+        updateAnimator();
+    }
+
+    private void moveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+        
+        if (hasHit)
+        {
+            moveTo(hit.point);
+        }
+    }
+
+    private void moveTo(Vector3 destination) {
+        GetComponent<NavMeshAgent>().destination = destination;
+    }
+
+    private void updateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+}
